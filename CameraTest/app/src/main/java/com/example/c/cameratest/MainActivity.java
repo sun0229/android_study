921f1b69baf8cdf41d2a6e9b19b45f8d6f675a73
sunny <very_goody@nate.com>
2015-01-31 16:59:32 +0900

    camera capture

--- CameraTest/app/src/main/java/com/example/c/cameratest/MainActivity.java
+++ CameraTest/app/src/main/java/com/example/c/cameratest/MainActivity.java
@@ -1,19 +1,64 @@
 package com.example.c.cameratest;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private final int CAMERA_TEST = 0;
+    String path = Environment.getExternalStorageDirectory().toString()+"/temp/imageTest.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Button captureButton = (Button)findViewById(R.id.capture);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(path)));
+                startActivityForResult(intent, CAMERA_TEST);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CAMERA_TEST){
+            if(resultCode == RESULT_OK){
+                File file = new File(path);
+                Bitmap captureBmp = null;
+
+                try {
+                    captureBmp = MediaStore.Images.Media.getBitmap(
+                            getContentResolver(), Uri.fromFile(file));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ImageView imgView = (ImageView)findViewById(R.id.imageView);
+                imgView.setImageBitmap(captureBmp);
+
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
