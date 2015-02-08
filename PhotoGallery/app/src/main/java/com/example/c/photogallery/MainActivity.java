f257615ca9ad9ec9bd56069031f418fc82003d91
sunny <very_goody@nate.com>
2015-02-08 10:29:36 +0900

    project created

---
+++ PhotoGallery/app/src/main/java/com/example/c/photogallery/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.c.photogallery;
+
+import android.support.v4.app.Fragment;
+
+
+public class MainActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
